8f42e08f37cc5cfda5ce73e2f005349768af5638
MrNobodyBlank <icapuyan18@gmail.com>
2018-09-13 09:13:18 +0800

    upload

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -75,4 +75,3 @@
         <?php if($this->session->flashdata('category_deleted')) : ?>
             <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_deleted').'</p>'; ?>
         <?php endif; ?>
-        
